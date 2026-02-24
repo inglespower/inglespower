2b5c4dfe1b3e6512f18663c5399fde2a72541079
inglespower <learnengish55@gmail.com>
2026-02-23 23:57:37 -0800

    Fix indentation for one minute warning

--- main.py
+++ main.py
@@ -63,8 +63,8 @@ async def process(request: Request):
 reply = generate_reply(speech)
 
 # aviso cuando queda 1 minuto
-if get_minutes(phone) == 1:
-resp.say("You have one minute remaining.", voice="alice")
+    if get_minutes(phone) == 1:
+        resp.say("You have one minute remaining.", voice="alice")
 
 # responder
 gather = Gather(
