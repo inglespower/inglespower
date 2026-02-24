d42b3cdfdadb21897570193cd1272e4b1caaa915
inglespower <learnengish55@gmail.com>
2026-02-23 19:23:33 -0800

    Add functions to get, add, and subtract minutes
    Implement functions to manage minutes for phone numbers.

---
+++ superbase_client.py
@@ -0,0 +1,28 @@
+from supabase import create_client
+from config import SUPABASE_URL, SUPABASE_KEY
+
+supabase = create_client(SUPABASE_URL, SUPABASE_KEY)
+
+def get_minutes(phone):
+res = supabase.table("minutes").select("*").eq("phone_number", phone).execute()
+if res.data:
+return res.data[0]["minutes_remaining"]
+return 0
+
+def add_minutes(phone, minutes):
+res = supabase.table("minutes").select("*").eq("phone_number", phone).execute()
+if res.data:
+current = res.data[0]["minutes_remaining"]
+supabase.table("minutes").update({"minutes_remaining": current + minutes}).eq("phone_number", phone).execute()
+else:
+supabase.table("minutes").insert({
+"phone_number": phone,
+"minutes_remaining": minutes
+}).execute()
+
+def subtract_minute(phone):
+m = get_minutes(phone)
+if m > 0:
+supabase.table("minutes").update({"minutes_remaining": m - 1}).eq("phone_number", phone).execute()
+return True
+return False
