fdc8b3dc2b664d490b2afbb787a909541156d4e3
inglespower <learnengish55@gmail.com>
2026-03-02 18:00:20 -0500

    Update config.py

--- config.py
+++ config.py
@@ -6,8 +6,9 @@ load_dotenv()
 class Config:
     OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
     ELEVENLABS_API_KEY = os.getenv("ELEVENLABS_API_KEY")
+    # Esta es la variable que debes tener en Render
+    ELEVENLABS_VOICE_ID = os.getenv("ELEVENLABS_VOICE_ID") 
     TELNYX_API_KEY = os.getenv("TELNYX_API_KEY")
-    TELNYX_MESSAGING_PROFILE_ID = os.getenv("TELNYX_MESSAGING_PROFILE_ID")
     SUPABASE_URL = os.getenv("SUPABASE_URL")
     SUPABASE_KEY = os.getenv("SUPABASE_KEY")
     PORT = int(os.getenv("PORT", 10000))
